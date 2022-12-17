faebb3f71314a47a4609d7b498979a2790fd93d6
0xWaleed <imwaleed@outlook.sa>
2022-12-18 01:46:36 +0300

    refactor: drop ts

---
+++ src/index.js
@@ -0,0 +1,18 @@
+export const I = new Proxy({}, {
+  get(target, key) {
+    const native = global[key];
+
+    if (native === undefined) {
+      throw Error(`'${key.toString()}' is not defined in global scope.`);
+    }
+
+    return (...args) => {
+      return new Promise((resolve) => {
+        setImmediate(() => {
+          const result = native(...args);
+          resolve(result);
+        });
+      });
+    };
+  }
+});
