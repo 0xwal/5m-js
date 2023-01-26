b390fe917303fe556a70b093639137c31951f2b6
0xWaleed <imwaleed@outlook.sa>
2023-01-27 01:25:41 +0300

    chore(types): add http type definition :wrench:

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -3,7 +3,17 @@ export {};
 interface rpc
 {
     [key: string]: Function;
+
     native: any;
+}
+
+declare type Header = { [key: string]: string }
+
+interface RequestOptions
+{
+    method: string;
+    headers: Header[];
+    data: any;
 }
 
 declare global
@@ -11,4 +21,8 @@ declare global
     const I: any;
 
     const rpc: rpc;
+
+    function httpRequest(url: string, req: RequestOptions);
+
+    function httpRequestJson(url: string, req: RequestOptions);
 }
